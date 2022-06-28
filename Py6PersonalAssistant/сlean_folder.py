01e0f0137a736495800746e40b39970730a2ec91
icoderp <igor.voyt98@gmail.com>
2022-06-28 19:40:29 +0300

    Update сlean_folder.py

--- Py6PersonalAssistant/сlean_folder.py
+++ Py6PersonalAssistant/сlean_folder.py
@@ -1,5 +1,5 @@
-from clean_folder_tools.file_parser import *
-from clean_folder_tools.normalize import normalize
+from Py6PersonalAssistant.clean_folder_tools.file_parser import *
+from Py6PersonalAssistant.clean_folder_tools.normalize import normalize
 from pathlib import Path
 import shutil
 
